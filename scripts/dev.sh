060050a335a21684ec296ee0e0b6bc1daf726ee2
Jim Heising <jheising@gmail.com>
2022-05-18 17:53:42 -0700

    Changes

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
 export API_ROOT="${API_ROOT:-./api}"
-export SCRIPT_DIR=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 export LOG_LEVEL="${LOG_LEVEL:-info}"
 
-echo "${API_ROOT}/**/*"
-
-ts-node-dev --watch "${API_ROOT}/**" --transpile-only "${SCRIPT_DIR}/dev-server.ts"
+ts-node-dev --watch "${API_ROOT}/**" --transpile-only "../scripts/dev-server.ts"
